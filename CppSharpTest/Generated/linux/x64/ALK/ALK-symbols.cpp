19992d22dd6adcc3e8c2e3cdba0425e331ecb7f9
Polar_Humenn <Polar_Humenn@trimble.com>
2020-05-02 10:39:38 -0400

    Conditional Remove Linux/Windows

--- CppSharpTest/Generated/linux/x64/ALK/ALK-symbols.cpp
+++
@@ -1,7 +0,0 @@
-#define _LIBCPP_DISABLE_VISIBILITY_ANNOTATIONS
-#define _LIBCPP_HIDE_FROM_ABI
-
-#include </home/polar/RiderProjects/CppSharpTest/CppSharpTest/Includes.hpp>
-#include <new>
-
-Test& (Test::*_0)(const Test&) = &Test::operator=;
